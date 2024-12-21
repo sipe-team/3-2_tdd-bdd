c654619d78568d7b240755faeaf4433bf890f4a7
JIHOON LEE <jihoon7705@gmail.com>
2024-12-21 12:46:05 +0900

    feature : useInfiniteScroll 기본 설정

--- jest.config.ts
+++ jest.config.ts
@@ -8,5 +8,14 @@ export default {
         '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
         '^@/(.*)$': '<rootDir>/src/$1'
     },
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testEnvironmentOptions: {
+        customExportConditions: [''],
+    },
+    globals: {
+        'ts-jest': {
+            tsconfig: 'tsconfig.json'
+        }
+    },
+    types: ['jest', '@types/jest', '@testing-library/jest-dom', '@jest/globals']
 };
---
+++ src/uesInfiniteScroll/index.tsx
@@ -0,0 +1,2 @@
+const useInfiniteScroll = () => {};
+export default useInfiniteScroll;
